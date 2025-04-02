17da72c2a6d4054e4253dd9c2a9071b772ca40f0
unknown <diegojf123@gmail.com>
2025-04-02 15:06:28 -0500

    mejoras

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
+require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./path/pixel-warrior.json');
-
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://pixel-warrior-13fcc-default-rtdb.firebaseio.com"
+    credential: admin.credential.cert({
+        "type": process.env.FIREBASE_TYPE,
+        "project_id": process.env.FIREBASE_PROJECT_ID,
+        "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+        "private_key": process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+        "client_id": process.env.FIREBASE_CLIENT_ID,
+        "auth_uri": process.env.FIREBASE_AUTH_URI,
+        "token_uri": process.env.FIREBASE_TOKEN_URI,
+        "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+        "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL,
+        "universe_domain": process.env.FIREBASE_UNIVERSE_DOMAIN
+    }),
+    databaseURL: process.env.FIREBASE_DATABASE_URL
 });
+
+console.log(process.env.FIREBASE_TYPE)
 
 const database = admin.database();
 const app = express();
